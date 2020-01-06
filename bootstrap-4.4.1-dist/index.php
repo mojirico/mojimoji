0a8906fe350545f6eab83526ae501ce0ae121626
mojimoji <milk78mayo@yahoo.co.jp>
2020-01-06 17:44:50 +0900

    Update index.php
    　書き足し

--- bootstrap-4.4.1-dist/index.php
+++ bootstrap-4.4.1-dist/index.php
@@ -1,10 +1,1 @@
-<?php
-  session_start();
-
-  if(!isset($_SESSION["user_name"])) {
-    $no_login_url = "index.php";
-    header("Location: {$no_login_url}");
-    exit;
-  }
-
-?>
+<?php require_once('header.php'); ?>
